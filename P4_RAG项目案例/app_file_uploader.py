d2a43d0f0f7bf77eb0b44d6a6a10a368c55fd389
RoxChen <1572890271@qq.com>
2026-04-03 16:09:17 +0800

    RAG项目

---
+++ P4_RAG项目案例/app_file_uploader.py
@@ -0,0 +1,28 @@
+"""
+基于streamlit完成WEB网页上传服务
+"""
+
+import streamlit as st
+#添加网页标题
+st.title("知识库更新服务")
+
+#file_uploader
+# type指定文件格式
+# accept_multiple_files是否接受多个文件上传
+uploader_file = st.file_uploader(
+    "请上传TXT文件",
+    type = ['txt'],
+    accept_multiple_files = False,
+)
+if uploader_file is not None:
+    #提取文件信息
+    file_name = uploader_file.name  #获取文件名
+    file_type = uploader_file.type  #获取文件类型
+    file_size = uploader_file.size/1024  #获取文件大小(KB)
+
+    st.subheader(f"文件名:{file_name}")
+    st.write(f"格式:{file_type} | 大小:{file_size:.2f} KB")
+    # 获取内容
+    # get_value->bytes->decode('utf-8')
+    text = uploader_file.getvalue().decode("utf-8")
+    st.write(text)
