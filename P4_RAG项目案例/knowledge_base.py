300330bca9d1ce23e73bab4f8b677cc1cbb628eb
RoxChen <1572890271@qq.com>
2026-04-06 20:15:45 +0800

    Integrate Chroma and text splitting functionality
    Added support for Chroma vector storage and text splitting.

--- P4_RAG项目案例/knowledge_base.py
+++ P4_RAG项目案例/knowledge_base.py
@@ -4,6 +4,10 @@
 import os
 import config_data as config
 import hashlib
+from langchain_chroma import Chroma
+from langchain_community.embeddings import DashScopeEmbeddings
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from datetime import datetime
 
 def check_md5(md5_str:str):
     """检查传入的md5字符串是否已经被处理过了
@@ -37,21 +41,69 @@ def get_string_md5(input_str:str,encoding='utf-8'):
     # 创建md5对象 md5_hex = hashlib.md5().update(input_str.encode(encoding=encoding)).hexdigest()
     md5_obj = hashlib.md5()           # 得到md5对象
     md5_obj.update(str_bytes)         # 更新内容(传入即将要转换的字节数组)
-    md5_hex = md5_obj.hexdigest()     # 得到md5的十六进制字符串
+    md5_hex = md5_obj.hexdigest()     # 得到md5的十六进制字符串,hex：十六进制
     return md5_hex
 
 class KnowledgeBaseService(object):
     def __init__(self):
-        self.chroma = None        #向量存储的实例 chroma向量库对象
-        self.spliter = None       #文本分割器的对象
+        os.makedirs(config.persist_directory,exist_ok=True)
+        #如果文件夹不存在则创建，如果存在则跳过
+        self.chroma = Chroma(
+            # 向量存储的实例 chroma向量库对象
+            collection_name=config.collection_name,  #数据库的表名
+            embedding_function=DashScopeEmbeddings(model="text-embedding-v4"), #默认为v1修改为v4
+            persist_directory=config.persist_directory,
+        )
+        self.spliter = RecursiveCharacterTextSplitter(
+            chunk_size = config.chunk_size, #表示分割后的每一个文本段最大长度
+            chunk_overlap = config.chunk_overlap, #连续文本段之间的字符重叠数量
+            separators=config.separator, #自然段落划分的符号
+            length_function = len, #使用python自带的len函数做长度统计依据
+        )       #文本分割器的对象
+
+    # knowledge_chunks:
+    # 存储文本片段，用于向量化存储前的数据准备
+    # 类型为list[str]
     def upload_by_str(self,data,filename):
         #将传入的字符串进行向量化，存入向量数据库中
-        pass
+        #先得到传入字符串的md5值
+        md5_hex = get_string_md5(data)
+        if check_md5(md5_hex):
+            return "[跳过]内容已经存在知识库中"
+        #大于设定最大长度则进行文本分割
+        if len(data)>config.max_split_char_number:
+            #变量名 :类型 = 值 ':'仅进行提示作用
+            knowledge_chunks: list[str] = self.spliter.split_text(data)
+            #返回的是一个列表
+        else:
+            knowledge_chunks = [data]
+
+        #metadata为每个文本块提供附加信息,便于后期检索，过滤和管理
+        metadata = {
+            "source":filename,
+            "create_time":datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
+            "operator":"小陈",
+        }
+
+        self.chroma.add_texts(    #内容加载到向量库中
+            # iterable->list\tuple
+            knowledge_chunks,
+            metadatas = [metadata for _ in knowledge_chunks],
+        )
+
+        #
+        save_md5(md5_hex)
+        return "[成功]内容已经成功载入向量库"
 
 if __name__ == '__main__':
-    save_md5("8e777f2c6f7f860da463bfb4d8e5b7c6")
-    print(check_md5("8e777f2c6f7f860da463bfb4d8e5b7c6"))
-    #md5特性:只要str一样,md5代码一定一样,并且不管多长md5长度一定固定
+    # md5特性:只要str一样,md5代码一定一样,并且不管多长md5长度一定固定
+    service = KnowledgeBaseService()
+    r = service.upload_by_str("凯尔希","testfile")
+    print(r)
+
+    #save_md5("8e777f2c6f7f860da463bfb4d8e5b7c6")
+    #print(check_md5("8e777f2c6f7f860da463bfb4d8e5b7c6"))
+
     #r1 = get_string_md5("凯尔希凯尔希凯尔希凯尔希凯尔希凯尔希凯尔希凯尔希凯尔希凯尔希凯尔希凯尔希凯尔希凯尔希凯尔希凯尔希凯尔希凯尔希凯尔希凯尔希凯尔希凯尔希凯尔希凯尔希凯尔希凯尔希凯尔希凯尔希凯尔希凯尔希凯尔希凯尔希凯尔希凯尔希凯尔希凯尔希凯尔希凯尔希凯尔希凯尔希凯尔希凯尔希凯尔希凯尔希")
     #r2 = get_string_md5("凯尔希")
     #r3 = get_string_md5("阿米娅")
