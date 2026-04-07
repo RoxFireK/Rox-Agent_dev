38c0d9d1a5952900e19d561afe9e4ad54640d383
RoxChen <1572890271@qq.com>
2026-04-07 09:25:35 +0800

    Update config_data.py

--- P4_RAG项目案例/config_data.py
+++ P4_RAG项目案例/config_data.py
@@ -1,2 +1,12 @@
 #需要多次修改的变量应单独设置文件进行保存和调用
 md5_path = "./md5.text"
+
+#Chroma
+collection_name = "rag"
+persist_directory = "./chroma_db"
+
+#spliter
+chunk_size = 1000
+chunk_overlap = 100
+separator = ["\n\n","\n",",",".","!","?","，","。","！","？"," "]
+max_split_char_number = 1000
