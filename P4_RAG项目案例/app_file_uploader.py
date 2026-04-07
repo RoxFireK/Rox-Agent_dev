616276a10a2aedfa0d44f46911eceb056a140ebc
RoxChen <1572890271@qq.com>
2026-04-07 09:26:25 +0800

    Update app_file_uploader.py

--- P4_RAG项目案例/app_file_uploader.py
+++ P4_RAG项目案例/app_file_uploader.py
@@ -1,12 +1,15 @@
 """
 基于streamlit完成WEB网页上传服务
 """
+import time
 
+#当页面元素发生变化，则代码重新执行一遍
 import streamlit as st
+from knowledge_base import KnowledgeBaseService
 #添加网页标题
 st.title("知识库更新服务")
 
-#file_uploader
+# file_uploader
 # type指定文件格式
 # accept_multiple_files是否接受多个文件上传
 uploader_file = st.file_uploader(
@@ -14,6 +17,11 @@ uploader_file = st.file_uploader(
     type = ['txt'],
     accept_multiple_files = False,
 )
+
+#session_state就是一个字典
+if "service" not in st.session_state:
+    st.session_state["service"] = KnowledgeBaseService()
+
 if uploader_file is not None:
     #提取文件信息
     file_name = uploader_file.name  #获取文件名
@@ -25,4 +33,21 @@ if uploader_file is not None:
     # 获取内容
     # get_value->bytes->decode('utf-8')
     text = uploader_file.getvalue().decode("utf-8")
-    st.write(text)
+
+    #在spinner内的代码执行过程中，会有一个转圈动画
+    with st.spinner("载入知识库中..."):
+        time.sleep(1)
+    result = st.session_state["service"].upload_by_str(text,file_name)
+    st.write(result)
+    #st.write(text)
+
+    #st.session_state["counter"] += 1
+    #里面用双引号外面用单引号,引号不能重复
+    #print(f'上传了{st.session_state["counter"]}个文件')
+
+#如何运行
+#打开终端
+#d:
+#cd D:\AIagent\P4_RAG项目案例
+#conda activate agent_dev
+#streamlit run app_file_uploader.py
